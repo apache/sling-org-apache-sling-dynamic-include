e55aab0f996844d430fe1e12e16befdd84ce7d88
Tomasz Niedzwiedz <tomasz.niedzwiedz@cognifide.com>
2018-05-28 19:20:53 +0200

    SLING-7742 - Prevent selector duplication when using nested components handled with SDI
      - Minor refactor to make the selector handling easier to unit test.
      - Unit tests for various combination of selector strings
      - Update the business logic to never add the same selector twice
      - Introduce an implementation package `org.apache.sling.dynamicinclude.impl` to encapsulate the implementation details and avoid changing the versions of exported packages
      - Update the pom version in preparation for a release.
    
    Minor update by rombert@apache.org - keep bundle version unchanged.
    
    Closes #5

---
+++ src/main/java/org/apache/sling/dynamicinclude/impl/UrlBuilder.java
@@ -0,0 +1,54 @@
+/*-
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.sling.dynamicinclude.impl;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.sling.api.request.RequestPathInfo;
+
+import java.util.Arrays;
+
+public final class UrlBuilder {
+
+
+    public static String buildUrl(String includeSelector, String resourceType, boolean synthetic, RequestPathInfo pathInfo) {
+        final StringBuilder builder = new StringBuilder();
+
+        final String resourcePath = pathInfo.getResourcePath();
+        builder.append(resourcePath);
+        String currentSelectorString = StringUtils.defaultString(pathInfo.getSelectorString());
+        if (pathInfo.getSelectorString() != null) {
+            builder.append('.').append(currentSelectorString);
+        }
+        if (includeSelectorNotAlreadyPresent(pathInfo.getSelectors(), includeSelector)) {
+            builder.append('.').append(includeSelector);
+        }
+        builder.append('.').append(pathInfo.getExtension());
+        if (synthetic) {
+            builder.append('/').append(resourceType);
+        } else {
+            builder.append(StringUtils.defaultString(pathInfo.getSuffix()));
+        }
+        return builder.toString();
+    }
+
+    private static boolean includeSelectorNotAlreadyPresent(String[] currentSelectors, String includeSelector) {
+        return !Arrays.asList(currentSelectors).contains(includeSelector);
+    }
+}
